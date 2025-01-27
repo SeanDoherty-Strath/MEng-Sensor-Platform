c798661686b634711bc31c2a7607fd83077b2ee9
Ross Inglis <r.inglis617@gmail.com>
2025-01-27 08:10:19 +0000

    Object detection & RGB cameras pi test setup

--- object_detection/object_detection.py
+++ object_detection/object_detection.py
@@ -1,132 +1,123 @@
 from ultralytics import YOLO
+
+# from picamera2 import Picamera2
+# from libcamera import controls
 import cv2
+from time import time
+import multiprocessing
+import json
 import math
-import time
 
 
-def bounding_box(img, box, labels):
-    # bounding box
-    x1, y1, x2, y2 = box.xyxy[0]
-    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # convert to int values
+def setup_cameras():
+    """Setup and start both cameras"""
+    camA = Picamera2(0)
+    camB = Picamera2(1)
 
-    # put box on image
-    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+    # Set image size
+    # config = camA.create_still_configuration({"size":()})
+    # camA.align_configuration(config)
+    # camB.align_configuration(config)
+    # camA.configure(config)
+    # camB.configure(config)
 
-    # confidence
-    confidence = math.ceil((box.conf[0] * 100)) / 100
-    # print("Confidence --->", confidence)
+    # Setup focus
+    camA.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+    camB.set_controls({"AfMode": controls.AfModeEnum.Continuous})
 
-    # class name
-    cls = int(box.cls[0])
-    # print("Class name -->", labels[cls])
+    # Start cameras
+    camA.start()
+    camB.start()
 
-    # object details
-    org = [x1, y1]
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    fontScale = 1
-    color = (255, 0, 0)
-    thickness = 2
-
-    cv2.putText(
-        img, f"{labels[cls]} -- {confidence}", org, font, fontScale, color, thickness
-    )
-    return img
+    return (camA, camB)
 
 
-def rgb_capture():
-    model = YOLO("yolo_models/yolo11n.pt")
-    count = 0
-    start = time.time()
+def capture(cams, save_dir=None):
+    """Triggers capture on the cameras {cams}. If {save_dir} specified, the images will be saved to {save_dir} as (timestamp)_#.jpg, otherwise the frames will be returned as a tuple."""
+    # TODO: Take multiple image captures and choose best to ensure non-blurry images used
+    frames = []
 
-    if CAPTURE_METHOD == "IMAGE":
-        img = cv2.imread("dog.jpg")
+    for i in range(len(cams)):
+        if save_dir != None:
+            cams[i].capture_file(save_dir + "/" + int(time()) + i)
+        else:
+            frames[i] = cams[i].capture_array("main")
 
-        results = model.predict(img)
-
-        for r in results:
-            boxes = r.boxes
-            for box in boxes:
-                img = bounding_box(img, box, model.names)
-
-        cv2.imshow("img", img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-    if CAPTURE_METHOD == "WEBCAM":
-        cap = cv2.VideoCapture(0)
-
-        while True:
-            ret, frame = cap.read()
-            results = model.predict(frame, stream=True)
-
-            for r in results:
-                boxes = r.boxes
-                for box in boxes:
-                    frame = bounding_box(frame, box, model.names)
-
-            # Calculate FPS
-            if count % FPS_AVERAGE_NUM_FRAMES == 0:
-                end = time.time()
-                fps = FPS_AVERAGE_NUM_FRAMES / (end - start)
-                start = time.time()
-
-            # Show the FPS
-            fps_text = f"{fps:.2f} FPS"
-            cv2.putText(
-                frame, fps_text, [20, 20], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1
-            )
-
-            count += 1
-
-            cv2.imshow("frame", frame)
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
-
-        cv2.destroyAllWindows()
-
-    elif CAPTURE_METHOD == "PICAM":
-        # Import picamera libraries
-        from picamera2 import Picamera2
-        from libcamera import controls
-
-        picam = Picamera2()
-        picam.start()
-        picam.set_controls({"AfMode": controls.AfModeEnum.Continuous})
-
-        while True:
-
-            frame = picam.capture_array()
-
-            results = model.predict(frame, stream=True)
-
-            for r in results:
-                boxes = r.boxes
-                for box in boxes:
-                    frame = bounding_box(frame, box, model.names)
-
-            # Calculate FPS
-            if count % FPS_AVERAGE_NUM_FRAMES == 0:
-                end = time.time()
-                fps = FPS_AVERAGE_NUM_FRAMES / (end - start)
-                start = time.time()
-
-            # Show the FPS
-            fps_text = f"{fps:.2f} FPS"
-            cv2.putText(
-                frame, fps_text, [20, 20], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1
-            )
-
-            count += 1
-
-            cv2.imshow("frame", frame)
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
-
-        cv2.destroyAllWindows()
+    return frames
 
 
-CAPTURE_METHOD = "WEBCAM"  # or "PICAM", "WEBCAM" or "IMAGE"
-FPS_AVERAGE_NUM_FRAMES = 20
+def object_detection(model, frame):
+
+    results = model.predict(frame)
+    # format results
+    return results
+
+
+def to_json(result):
+    """Takes ultralytics result and returns json format"""
+    pass
+
 
 if __name__ == "__main__":
-    rgb_capture()
+    # Setup cameras and capture images
+    # cams = setup_cameras()
+    # capture(cams, "./captures/")
+
+    # Setup object detection model
+    model = YOLO("yolo_models/yolo11n.pt")
+
+    # Grab test images
+    img1 = cv2.imread("A_image1.jpg")
+    img2 = cv2.imread("A_image2.jpg")
+
+    # # Run sequentially
+    start_time = time()
+    results = object_detection(model, img1)
+    # _ = object_detection(model, img2)
+    print("###################################################################")
+    print(f"Time taken to process two images sequentially {time()-start_time}s")
+    print("###################################################################")
+
+    print(type(results[0]))
+
+    # for r in results:
+    #     r.save_txt("output.txt", True)
+    #     r.save_crop("./")
+
+    # json_result = results[0].to_json()
+    # print(json_result)
+
+    # Export results to json
+    data = []
+    for b in results[0].boxes:
+        data.append(
+            {
+                "coords": list(map(int, b.xywh[0].tolist())),
+                "RGB Classification": [
+                    model.names[int(b.cls[0])],
+                    math.ceil((b.conf[0] * 100)) / 100,
+                ],
+            }
+        )
+
+    with open("results.json", mode="w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4)
+
+    # Run with multiprocessing
+    # p1 = multiprocessing.Process(
+    #     target=object_detection,
+    #     args=(model, img1,),
+    # )
+    # p2 = multiprocessing.Process(
+    #     target=object_detection,
+    #     args=(model, img2,),
+    # )
+
+    # start_time = time()
+    # p1.start()
+    # p2.start()
+    # p1.join()
+    # p2.join()
+    # print("###################################################################")
+    # print(f"Time taken to process two images concurrently {time()-start_time}s")
+    # print("###################################################################")
