0c3c224de8a0825854336f171941ef463484c973
warda <56481100+andrewfward@users.noreply.github.com>
2025-01-28 15:07:37 +0000

    Added test images and updates to send and receive script

--- comms/send.py
+++ comms/send.py
@@ -2,6 +2,41 @@
 
 import os
 import socket
+from os import listdir
+
+def list_images(folder_path, client_socket):
+    # get the path/directory
+        images = [f for f in os.listdir(folder_path) if f.endswith(('.png', '.jpg', '.jpeg'))]
+        if not images:
+            print("No images found")
+            client_socket.close()
+            return
+
+        print(len(images))
+        client_socket.sendall(f"{len(images)}")
+
+        for image in images:
+            filesize = os.path.getsize(image)
+            
+            filename = os.path.basename(image)
+
+            client_socket.sendall(f"{filename}|{filesize}".encode())
+
+            # Wait for acknowledgment
+            ack = client_socket.recv(1024).decode()
+            if ack != "READY":
+                print("Server is not ready to receive the file.")
+                return
+    
+            # Send the image file
+            with open(folder_path, "rb") as file:
+                while (chunk := file.read(1024)):
+                    client_socket.sendall(chunk)
+            
+            print(f"Image {filename} sent successfully!")
+        
+        print("All images sent!")
+        return
 
 def send_images(path, ip, port):
     try:
@@ -10,6 +45,7 @@ def send_images(path, ip, port):
         client_socket.connect((ip, port))
         print(f"Connected to {ip}:{port}")
 
+        list_images("./", client_socket)
     
     except Exception as e:
         print(f"Error: {e}")
@@ -18,8 +54,9 @@ def send_images(path, ip, port):
         client_socket.close()
 
 if __name__ == "__main__":
-    ip = "127.0.0.1"
+    ip = "192.168.1.144"
+    test_ip = "10.12.71.113"
     port = 5001
     path = ""
 
-    send_images(path, ip, port)
+    send_images(path, test_ip, port)
